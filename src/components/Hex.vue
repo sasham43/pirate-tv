5128826825a79edbb7a96d6a8cbc75af24429570
sasham43 <sashasemail@gmail.com>
2020-01-15 22:54:48 -0500

    add hex component

---
+++ src/components/Hex.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="hex">
+
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+
+.hex {
+    margin-top: 30px;
+    width: 104px;
+    height: 60px;
+    background-color: #ec6e2f;
+    position: relative;
+    display: inline-block;
+    margin-left: 4px;
+}
+.hex:before {
+    content: " ";
+    width: 0; height: 0;
+    border-bottom: 30px solid #ec6e2f;
+    border-left: 52px solid transparent;
+    border-right: 52px solid transparent;
+    position: absolute;
+    top: -30px;
+    left: 0;
+}
+.hex:after {
+    content: "";
+    width: 0;
+    position: absolute;
+    bottom: -30px;
+    border-top: 30px solid #ec6e2f;
+    border-left: 52px solid transparent;
+    border-right: 52px solid transparent;
+    left: 0;
+}
+</style>
--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -2,19 +2,26 @@
     <div class="menu">
         Menu
         <div class="hex-container">
-            <div class="hex">
+            <Hex></Hex>
+            <Hex></Hex>
+            <!-- <div class="hex">
 
             </div>
             <div class="hex">
 
-            </div>
+            </div> -->
         </div>
     </div>
 </template>
 
 <script>
+import Hex from './Hex.vue'
+
 export default {
-    name: 'Menu'
+    name: 'Menu',
+    components: {
+        Hex
+    }
 }
 </script>
 
@@ -30,35 +37,6 @@ body {
     width: 100%;
     padding: 25px;
     min-height: 300px;
-}
-
-.hex {
-    margin-top: 30px;
-    width: 104px;
-    height: 60px;
-    background-color: #e24f33;
-    position: relative;
-    display: inline-block;
-}
-.hex:before {
-    content: " ";
-    width: 0; height: 0;
-    border-bottom: 30px solid #e24f33;
-    border-left: 52px solid transparent;
-    border-right: 52px solid transparent;
-    position: absolute;
-    top: -30px;
-    left: 0;
-}
-.hex:after {
-    content: "";
-    width: 0;
-    position: absolute;
-    bottom: -30px;
-    border-top: 30px solid #e24f33;
-    border-left: 52px solid transparent;
-    border-right: 52px solid transparent;
-    left: 0;
 }
 
 
