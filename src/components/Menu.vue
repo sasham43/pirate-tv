321bcced6d941ce7c4e8d0cc3b5efec016d476f9
sasham43 <sashasemail@gmail.com>
2020-01-15 22:46:43 -0500

    add menu

---
+++ src/components/Menu.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="menu">
+        menu stuff
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Menu'
+}
+</script>
+
+<style>
+body {
+    background: #000;
+}
+.menu {
+    color: #e24f33;
+}
+
+
+</style>
