07accb82297f3ae478992889997b19cd09fbc5db
sasham43 <sashasemail@gmail.com>
2020-01-15 22:52:08 -0500

    add two hexes

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,6 +1,14 @@
 <template>
     <div class="menu">
-        menu stuff
+        Menu
+        <div class="hex-container">
+            <div class="hex">
+
+            </div>
+            <div class="hex">
+
+            </div>
+        </div>
     </div>
 </template>
 
@@ -16,6 +24,41 @@ body {
 }
 .menu {
     color: #e24f33;
+}
+
+.hex-container {
+    width: 100%;
+    padding: 25px;
+    min-height: 300px;
+}
+
+.hex {
+    margin-top: 30px;
+    width: 104px;
+    height: 60px;
+    background-color: #e24f33;
+    position: relative;
+    display: inline-block;
+}
+.hex:before {
+    content: " ";
+    width: 0; height: 0;
+    border-bottom: 30px solid #e24f33;
+    border-left: 52px solid transparent;
+    border-right: 52px solid transparent;
+    position: absolute;
+    top: -30px;
+    left: 0;
+}
+.hex:after {
+    content: "";
+    width: 0;
+    position: absolute;
+    bottom: -30px;
+    border-top: 30px solid #e24f33;
+    border-left: 52px solid transparent;
+    border-right: 52px solid transparent;
+    left: 0;
 }
 
 
