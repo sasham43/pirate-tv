d6cb802230deb99845fe96141d83178952940824
sasham43 <sashasemail@gmail.com>
2020-01-17 10:57:05 -0500

    add router

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,33 @@
 <template>
   <div id="app">
-    <Menu>
-
-    </Menu>
+    <!-- <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div> -->
+    <router-view/>
   </div>
 </template>
 
-<script>
-import Menu from './components/Menu.vue'
-
-export default {
-  name: 'app',
-  components: {
-    Menu
-  }
-}
-</script>
-
 <style>
 #app {
-  /* font-family: 'Avenir', Helvetica, Arial, sans-serif; */
   font-family: 'VT323', 'Courier New', monospace;
   font-size: 24px;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <Menu>
+
+    </Menu>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Menu from '@/components/Menu.vue'
+
+export default {
+  name: 'home',
+  components: {
+    Menu
+  }
+}
+</script>
