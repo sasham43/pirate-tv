8d9e3f02d4b1bac585180e161146e29945cb1b9b
sasham43 <sashasemail@gmail.com>
2020-01-18 12:23:17 -0500

    add button styles

--- src/App.vue
+++ src/App.vue
@@ -30,4 +30,21 @@
 #nav a.router-link-exact-active {
   color: #42b983;
 }
+
+button {
+    padding: 5px;
+    color: #ec6e2f;
+    font-family: 'VT323', 'Courier New', monospace;
+    font-size: 24px;
+    text-shadow: 0px 0px 4px;
+    outline: none;
+    background: transparent;
+    border: solid 2px #ec6e2f;
+    margin-top: 15px;
+}
+button:hover {
+    color: #28afb0;
+    border-color: #28afb0;
+    cursor: pointer;
+}
 </style>
